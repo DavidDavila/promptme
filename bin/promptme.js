daebd409732f4039e2d9611ef2b2f156a7621c44
David Jesús Dávila Pino <djdavila@bankinter.com>
2025-05-09 14:14:20 +0200

    da

--- bin/promptme.js
+++ bin/promptme.js
@@ -2,21 +2,30 @@
 
 const { program } = require('commander');
 const { generatePromptFiles } = require('../lib/generate');
+const { resetAll } = require('../lib/reset');
 
 program
+  .name('promptme')
+  .description('CLI para generar contexto de código para IA')
   .option('-m, --maxlength <bytes>', 'Tamaño máximo por archivo', '40000')
   .option('-o, --output <base>', 'Nombre base de archivo', 'project_prompt')
   .option('-f, --format <format>', 'Formato: txt, md, json', 'txt')
-  .option('-i, --include <folders>', 'Carpetas a incluir', val => val.split(','), [])
+  .option('-i, --include <folders>', 'Carpetas a incluir (ej: src,test)', val => val.split(','), [])
   .option('--template <file>', 'Usar plantilla personalizada')
-  .option('--summary', 'Generar resumen del proyecto') // opcional si lo implementaste
-  .parse(process.argv);
+  .option('--summary', 'Generar resumen del proyecto')
+  .action((opts) => {
+    generatePromptFiles({
+      maxLength: parseInt(opts.maxlength),
+      outputBase: opts.output,
+      format: opts.format,
+      includeFolders: opts.include,
+      templatePath: opts.template
+    });
+  });
 
-const opts = program.opts();
-generatePromptFiles({
-  maxLength: parseInt(opts.maxlength),
-  outputBase: opts.output,
-  format: opts.format,
-  includeFolders: opts.include,
-  templatePath: opts.template
-});
+program
+  .command('reset')
+  .description('Restablece la plantilla y el archivo de ignore por defecto')
+  .action(resetAll);
+
+program.parse();
